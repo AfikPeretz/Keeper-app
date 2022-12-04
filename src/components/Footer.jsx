7513e445678f36879d936dba0d6b251fe26f66c1
AfikPeretz <afikperetz5235@gmail.com>
2022-12-04 11:01:42 +0200

    Start to build the body

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    const Year = new Date().getFullYear();
+    return(
+        <footer>
+            <p>
+                copyright {Year}
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
